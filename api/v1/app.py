3dc4d0f5c6162c84242d9bba5326271d94a39854
Juan Garcia <juanlds621@gmail.com>
2022-12-14 03:35:53 +0000

    error 404 not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
     app
 """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,11 @@ app.register_blueprint(app_views)
 def close_session(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
