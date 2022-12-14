58990808257eb69490fc9475a795ed7884323edb
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-12-14 15:18:29 -0500

    correction 9.6 pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -44,7 +44,7 @@ def create_amenity():
         abort(400, 'Missing name')
     new_amenity = Amenity(**new_amenity)
     new_amenity.save()
-    return jsonify(new_amenity.to_dict()), 201    
+    return jsonify(new_amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
